66e85285697850f0a57dbe527e23411e8e6743d4
fatimadekmak <fatima.k.dokmak@gmail.com>
2023-02-24 10:00:09 +0200

    added another slide in landing section

--- index1.php
+++ index1.php
@@ -69,6 +69,29 @@
 			</div>
 		</div>
 
+		<div class="slider-item">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row slider-text align-items-center" data-scrollax-parent="true">
+
+                    <div class="col-md-6 col-sm-12 ftco-animate">
+                        <span class="subheading">Delicious</span>
+                        <h1 class="mb-4">Italian Cuizine</h1>
+                        <p class="mb-4 mb-md-5">A small river named Duden flows by their place and supplies it with the
+                            necessary regelialia.</p>
+                        <p><a href="#" class="btn btn-primary p-3 px-xl-4 py-xl-3">Browse Italian Cuisine</a> <a
+                                href="#" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">Browse Other
+                                Cuisines</a></p>
+                    </div>
+                    <div class="col-md-6 ftco-animate">
+                        <img src="images/bg_1.png" class="img-fluid" alt="">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        </div>
+
 	</section>
 
 
